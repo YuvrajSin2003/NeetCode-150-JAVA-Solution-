315cb59e1bd17b524be712ef891273c18e1d4fa9
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2026-03-30 20:37:05 +0530

    Two sum 2

---
+++ TwoPointers/TwoSum2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        int[] ans = new int[2];
+        while(left < right){
+            if(numbers[right] + numbers[left] == target){
+                return new int[]{left + 1, right + 1};
+            }else if (numbers[right] + numbers[left] > target) {
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
