25a7bd468a76032cdd41fdc50bf85598e0de4073
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2026-04-08 19:14:04 +0530

    Search a 2D Matrix

---
+++ Binary Search/SearchIn2DMatrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int left = 0;
+
+        int right = m*n -1 ;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            int val = matrix[mid/m][mid%n];
+            if(val == target){
+                return true;
+            }else if(val > target){
+                right = mid - 1;
+            }else{
+                left = mid +1;
+            }
+        }
+    return false;
+       
+    }
+}
